c6f1ea467119e8b953dac074834b06582166d91f
Alaa Hamamo <alaahamamo7@gmail.com>
2026-09-03 22:09:31 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Movies());
+}
+
+class Movies extends StatelessWidget {
+  const Movies({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
