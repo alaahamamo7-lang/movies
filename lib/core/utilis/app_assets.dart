2abcf88b4e4bd2e36326b44ca1ec0a8653fa4648
DESKTOP-SN0LLQ6\marwa <marwangamal931@gmail.com>
2026-09-08 21:13:33 +0300

    completed register and forget_password screens

---
+++ lib/core/utilis/app_assets.dart
@@ -0,0 +1,14 @@
+abstract final class AppAssets {
+  static const nameIcon = "assets/icons/name_icon.svg";
+  static const phoneIcon = "assets/icons/phone_icon.svg";
+  static const eyesOffIcon = "assets/icons/eyes_off_icon.svg";
+  static const emailIcon = "assets/icons/email_icon.svg";
+  static const passwordIcon = "assets/icons/password_icon.svg";
+  static const egyptIcon = "assets/icons/EG.svg";
+  static const englishIcon = "assets/icons/LR.svg";
+  static const forgetPasswordBanner =
+      "assets/images/Forgot password-banner.svg";
+  static const avatar1 = "assets/images/avatar_1.png";
+  static const avatar2 = "assets/images/avatar_2.png";
+  static const avatar3 = "assets/images/avatar_3.png";
+}
