6b31a416c008314a77f39f77c832511c8b9d649c
Alaa Hamamo <alaahamamo7@gmail.com>
2026-09-10 01:55:39 +0300

    Avater Assetes and CricleAvater UI

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -5,4 +5,15 @@ abstract final class AppAssets {
   static const String svgPassword = "assets/icons/password.svg";
   static const String svgEyeOff = "assets/icons/eyeOff.svg";
   static const String svgGoogle = "assets/icons/icon_google.svg";
+  static const String svgPhone = "assets/icons/phone.svg";
+  //--------------------------------------------------------------------
+  static const String avatar1 = "assets/images/avatar1.png";
+  static const String avatar2 = "assets/images/avatar2.png";
+  static const String avatar3 = "assets/images/avatar3.png";
+  static const String avatar4 = "assets/images/avatar4.png";
+  static const String avatar5 = "assets/images/avatar5.png";
+  static const String avatar6 = "assets/images/avatar6.png";
+  static const String avatar7 = "assets/images/avatar7.png";
+  static const String avatar8 = "assets/images/avatar8.png";
+  static const String avatar9 = "assets/images/avatar9.png";
 }
--- lib/features/update_profile/ui/update_profile.dart
+++ lib/features/update_profile/ui/update_profile.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:movies/core/constants/app_assets.dart';
 import 'package:movies/core/constants/app_text.dart';
 
 class UpdateProfileScreen extends StatelessWidget {
   static const String routeName = "/updateProfile";
+  String selectedAvatar = AppAssets.avatar1;
+  List<String> avatarList = [
+    AppAssets.avatar1,
+    AppAssets.avatar2,
+    AppAssets.avatar3,
+    AppAssets.avatar4,
+    AppAssets.avatar5,
+    AppAssets.avatar6,
+    AppAssets.avatar8,
+    AppAssets.avatar9,
+  ];
 
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
+    double sizeW = MediaQuery.sizeOf(context).width;
     return Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.arrow_back, color: theme.colorScheme.primary),
@@ -18,7 +31,16 @@ class UpdateProfileScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Column(children: [CircleAvatar()]),
+      body: Column(
+        children: [
+          Center(
+            child: CircleAvatar(
+              backgroundImage: AssetImage(selectedAvatar),
+              radius: sizeW * 0.16,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
